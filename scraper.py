11501b9b8ed810f8d5858f2a451de1b8fd820360
Zguy57 <91798005+Zguy57@users.noreply.github.com>
2021-11-17 10:16:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-def scrape_objects(obj_type,link):
-        ''' This function retrieves all of the instances of a certain object type. '''
+def scrape_objects_text(obj_type,link):
+        ''' This function retrieves all instances of a certain object type. '''
         page = requests.get(link)
         soup = BeautifulSoup(page.content,"lxml")
         results = soup.find_all(obj_type)
         for i in range(len(results)):
                 results[i] = results[i].text
+        return results
+
+def scrape_objects_attr(obj_type,link,attr):
+        ''' This function retrieves the value of an attribute of all instances of a certain object type. '''
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content,"lxml")
+        results = soup.find_all(obj_type)
+        for i in range(len(results)):
+                results[i] = results[i].get(attr)
         return results
 
 
