546267545101ff5404303d489a4a9611b8e3c028
Shahar Y <91798005+Zguy57@users.noreply.github.com>
2022-04-06 07:19:25 +0000

    a lot

--- ui.py
+++ ui.py
@@ -5,91 +5,174 @@ from user_system import *
 from extra import *
 
 app = Flask(__name__)
-app.secret_key = "abcdefg"
+
 
 @app.route("/")
 def home_page():
-        return redirect("/main/0")
+    return redirect("/main")
 
-def manage_scraping(form,numofattrs):
-        rules = {}
-        attrs = {}
-        attrnames = []
-        results = []  
-        if form["objectType"]:
-                        rules["objectType"] = form["objectType"]
-        if form["attr"] and form["attrVal"]:
-                rules["attr"] = [form["attr"],form["attrVal"]]
-        for i in range(int(numofattrs)):
-                attrnames.append(form[f"attrToScrape{i}"])
-        for i,rule in enumerate(rules):
-                results.append([])
-                if rule == "objectType":
-                        for attrname in attrnames:
-                                  for instance in scraper.scrape_object_by_type(rules[rule],form["link"],attrname):
-                                          results[i].append({attrname:instance})
-                elif rule == "attr":
-                        for attrname in attrnames:
-                                for instance in scraper.scrape_object_by_attr(rules[rule][0],rules[rule][1],form["link"],attrname):
-                                          results[i].append({attrname:instance})
+
+def manage_scraping(form, final_stage):
+    rules = {}
+    attrs = []
+    attrnames = form["attrsToScrape"].split("|")
+    results = []
+    if form["objectType"]:
+        rules["objectType"] = form["objectType"]
+    if form["attr"] and form["attrVal"]:
+        rules["attr"] = [form["attr"], form["attrVal"]]
+    if form["link"] and final_stage:
+        for i, rule in enumerate(rules):
+            results.append([])
+            if rule == "objectType":
+                for attrname in attrnames:
+                    for instance in scraper.scr_obj_by_type(rules[rule],
+                                                            attrname,
+                                                            link=form["link"]):
+                        results[i].append({attrname: instance})
+            elif rule == "attr":
+                for attrname in attrnames:
+                    for instance in scraper.scr_obj_by_attr(rules[rule][0],
+                                                            rules[rule][1],
+                                                            attrname,
+                                                            link=form["link"]):
+                        results[i].append({attrname: instance})
         attrs = format_lst(only_dups(results))
-        return attrs
+    elif form["link"]:
+        for i, rule in enumerate(rules):
+            results.append([])
+            if rule == "objectType":
+                for instance in scraper.find_obj_by_type(rules[rule],
+                                                         link=form["link"]):
+                    results[i].append(instance)
+            elif rule == "attr":
+                for instance in scraper.find_obj_by_attr(rules[rule][0],
+                                                         rules[rule][1],
+                                                         link=form["link"]):
+                    results[i].append(instance)
+        attrs = only_dups(results)
+    elif form["submit"] != "Submit" and final_stage:
+        for i, rule in enumerate(rules):
+            results.append([])
+            if rule == "objectType":
+                for attrname in attrnames:
+                    for instance in scraper.scr_obj_by_type(
+                            rules[rule], attrname, tree=form["submit"]):
+                        results[i].append({attrname: instance})
+            elif rule == "attr":
+                for attrname in attrnames:
+                    for instance in scraper.scr_obj_by_attr(
+                            rules[rule][0],
+                            rules[rule][1],
+                            attrname,
+                            tree=form["submit"]):
+                        results[i].append({attrname: instance})
+        attrs = format_lst(only_dups(results))
+    elif form["submit"] != "Submit":
+        for i, rule in enumerate(rules):
+            results.append([])
+            if rule == "objectType":
+                for instance in scraper.find_obj_by_type(rules[rule],
+                                                         tree=form["submit"]):
+                    results[i].append(instance)
+            elif rule == "attr":
+                for instance in scraper.find_obj_by_attr(rules[rule][0],
+                                                         rules[rule][1],
+                                                         tree=form["submit"]):
+                    results[i].append(instance)
+        attrs = only_dups(results)
+    return attrs
 
-@app.route("/main/<numofattrs>",methods=["GET","POST"])
-def main(numofattrs):
-        attrs={}
-        if request.method == "POST":
-                if request.form["numofattrstoscrape"]:
-                        return redirect(f"/main/{request.form['numofattrstoscrape']}")
-                attrs = manage_scraping(request.form, numofattrs)
-        return render_template("main.html", attrs=attrs, num_of_attrs=int(numofattrs), redirect_url=f"/main/{int(numofattrs)}", username="Guest")
 
-@app.route("/login",methods=["GET","POST"])
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    attrs = {}
+    final_stage = False
+    if request.method == "POST":
+        if request.form["attrsToScrape"]:
+            final_stage = True
+        attrs = manage_scraping(request.form, final_stage)
+    return render_template("main.html",
+                           attrs=attrs,
+                           redirect_url=f"/main",
+                           username="Guest",
+                           final_stage=final_stage)
+
+
+@app.route("/login", methods=["GET", "POST"])
 def log_in():
-        return render_template("connect.html", operation="Log in", message="", url="/welcome_user")
+    return render_template("connect.html",
+                           operation="Log in",
+                           message="",
+                           url="/welcome_user",
+                           show_username=True,
+                           show_password=True,
+                           show_email=False)
 
-@app.route("/register",methods=["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-        message = ""
-        if request.method == "POST":
-                if User.register(request.form["username"], request.form["password"]):
-                        message = "registered successfully!"
-                else:
-                        message = "Username already taken!"
-        return render_template("connect.html", operation="Register", message=message)
+    message = ""
+    if request.method == "POST":
+        if User.register(request.form["username"], request.form["password"],
+                         request.form["email"]):
+            message = "Registered successfully!"
+        else:
+            message = "Failed to register!"
+    return render_template("connect.html",
+                           operation="Register",
+                           message=message,
+                           url="/register",
+                           show_username=True,
+                           show_password=True,
+                           show_email=True)
 
-@app.route("/welcome_user",methods=["GET","POST"])
+
+@app.route("/welcome_user", methods=["GET", "POST"])
 def welcome_user():
-        if request.method == "POST":
-                user = User.get_user(request.form["username"], request.form["password"])
-                if user:
-                        resp = make_response(render_template("welcome.html", username=request.form["username"], history=user.get_history()))
-                        resp.set_cookie("username", request.form["username"])
-                        return resp
-                else:
-                        return render_template("failed.html")
+    if request.method == "POST":
+        user = User.get_user(request.form["username"],
+                             request.form["password"])
+        if user:
+            resp = make_response(
+                render_template("welcome.html",
+                                username=request.form["username"],
+                                history=user.get_history()))
+            resp.set_cookie("username", request.form["username"])
+            return resp
         else:
-                return "It seems like you're messing up with urls. Don't you?"
+            return render_template("failed.html")
+    else:
+        return "It seems like you're messing up with urls. Don't you?"
 
-@app.route("/main_user", methods=["GET","POST"])
+
+@app.route("/main_user", methods=["GET", "POST"])
 def main_user():
-        if request.method == "POST":
-                if "objectType" not in request.form or "num_of_attrs" in request.form:
-                        resp = make_response(render_template("main.html", attrs={}, num_of_attrs=int(request.form["num_of_attrs"]), redirect_url="/main_user"))
-                        resp.set_cookie("num_of_attrs", request.form["num_of_attrs"])
-                        return resp
-                attrs = manage_scraping(request.form, int(request.cookies.get("num_of_attrs")))
+    if request.method == "POST":
+        final_stage = False
+        if request.form["attrsToScrape"]:
+            final_stage = True
+        attrs = manage_scraping(request.form,
+                                final_stage)
+        if final_stage:
                 Online[request.cookies.get("username")].log_scrape(attrs)
-                return render_template("main.html", attrs=attrs, num_of_attrs=int(request.cookies.get("num_of_attrs")), redirect_url="main_user")
-        else:
-                return "It seems like you're messing up with urls. Don't you?" 
+        return render_template("main.html",
+                               attrs=attrs,
+                               redirect_url="main_user", final_stage=final_stage)
+    else:
+        return "It seems like you're messing up with urls. Don't you?"
 
-@app.route("/view_history", methods=["GET","POST"])
+
+@app.route("/view_history", methods=["GET", "POST"])
 def view_history():
+    if request.cookies.get("username")[0] == "@":
         historylst = []
         for user in db:
-                historylst.append((user,dict(json.loads(db[user]))["_history"]))
+            historylst.append((user, dict(json.loads(db[user]))["_history"]))
         return render_template("view_history.html", users=historylst)
+    else:
+        return "Not enough permissions!"
+
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
--- user_system.py
+++ user_system.py
@@ -1,39 +1,54 @@
 from replit import db
 from extra import *
 import json
+import re
 
 Online = {}
 
-class User():
+SPECIAL_CHARACTERS = ("@", "!")
+EMAIL_PATTERN = "[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+"
 
-        def __init__(self, username: str, password: str, history: str):
+class User():
+      
+        def __init__(self, username: str, password: str, email: str, history: str):
                 self._username = username
                 self._password = encrypt(username, password)
+                self._email = email
                 self._history = history
                 Online[username] = self
 
         def get_user(username: str, password: str):
+                '''This is a class function that retrieves a User object if the username and the password match what is found on the replit database.'''
                 if username in db and json.loads(db[username])["_password"] == encrypt(username,password) and username[0] != "!":
                         user = json.loads(db[username])
-                        return User(username, password, user["_history"])
+                        return User(username, password, user["_email"], user["_history"])
 
-        def register(username: str, password: str):
-                if not username in db and username[0] != "@" and username[0] != "!":
-                        db[username] = json.dumps(User(username, password, []).__dict__)
+        def register(username: str, password: str, email: str):
+                '''This is a class function that registers a new user if it does not exist on the replit database.'''
+                if not username in db and username[0] not in SPECIAL_CHARACTERS and re.fullmatch(EMAIL_PATTERN, email):
+                        db[username] = json.dumps(User(username, password, email, []).__dict__)
                         return True
                 return False
 
         def log_scrape(self, event: str):
+                '''This is an object function that logs a new scrape that a user made in the replit database.'''
                 self._history.append(event)
                 db[self._username] = json.dumps(self.__dict__)
 
         def get_history(self):
+                '''This is an object function that retrieves a user's history.'''
                 return self._history
 
         def get_username(self):
+                '''This is an object function that retrieves a user's username.'''
                 return self._username
 
+        def get_email(self):
+                '''This is an object function that retrieves a user's email.'''
+                return self._email
+          
         def log_out(self):
+                '''This is an object function that logs out a user'''
                 del Online[self._username]
         
 class Manager(User):
